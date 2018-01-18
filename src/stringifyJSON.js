8fec7542194d55b26b1fbb4f437efa443d6355ee
jorgelerma <jorge@jorgelerma.com>
2018-01-18 12:31:07 -0600

    Add method

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,40 @@
 
 // but you don't so you're going to write it from scratch:
 
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var arrOfKeyVals = [],
+    arrVals = [],
+    objKeys = [];
+  if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null)
+    return '' + obj;
+  else if (typeof obj === 'string')
+    return '"' + obj + '"';
+  else if (Array.isArray(obj)) {
+    if (obj[0] === undefined)
+      return '[]';
+    else {
+      obj.forEach(function(el) {
+        arrVals.push(stringifyJSON(el));
+      });
+      return '[' + arrVals + ']';
+    }
+  }
+  else if (obj instanceof Object) {
+    objKeys = Object.keys(obj);
+    objKeys.forEach(function(key) {
+      var keyOut = '"' + key + '":';
+      var keyValOut = obj[key];
+      if (keyValOut instanceof Function || typeof keyValOut === undefined)
+        arrOfKeyVals.push('');
+      else if (typeof keyValOut === 'string')
+        arrOfKeyVals.push(keyOut + '"' + keyValOut + '"');
+      else if (typeof keyValOut === 'boolean' || typeof keValOut === 'number' || keyValOut === null)
+        arrOfKeyVals.push(keyOut + keyValOut);
+      else if (keyValOut instanceof Object) {
+        arrOfKeyVals.push(keyOut + stringifyJSON(keyValOut));
+      }
+    });
+    return '{' + arrOfKeyVals + '}';
+  }
 };
